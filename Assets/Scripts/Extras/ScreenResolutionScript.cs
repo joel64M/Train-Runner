35e16de1c05d5a58361a836706938abf3b83ff26
Joel Camilo Fernandes <torn3do64@gmail.com>
2019-08-05 12:28:33 +0530

    Initial commit

---
+++ Assets/Scripts/Extras/ScreenResolutionScript.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+
+public class ScreenResolutionScript : MonoBehaviour
+{
+    public static ScreenResolutionScript instance;
+    [Range(30,100)]
+    public int resolutionPercentage=75;
+
+    private void Awake()
+    {
+        if (instance !=null && instance!=this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+           // DontDestroyOnLoad(this.gameObject);
+            int height = (int)(Screen.currentResolution.height * resolutionPercentage) / 100;
+            int width = (int)(Screen.currentResolution.width * resolutionPercentage) / 100;
+            //print(width);
+            //print(height);
+            Screen.SetResolution(width, height, true);
+            DontDestroyOnLoad(this.gameObject);
+        }
+  
+    }
+
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using PathCreation.Examples;
+public class PlayerScript : MonoBehaviour
+{
+    PathFollower pf;
+    GameManagerScript gms;
+
+    float speed;
+
+    private void Start()
+    {
+        UIManagerScript.instance.mainPlayerStats = GetComponent<Statistics>();
+
+        gms = GameManagerScript.instance;
+        pf = GetComponent<PathFollower>();
+        speed = gms.speed;
+    }
+
+
+    private void Update()
+    {
+        if (gms.isGameStart)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                pf.speed = 0;
+            }
+            if(Input.GetMouseButtonUp(0))
+            {
+                pf.speed = speed;
+            }
+        }
+
+    }
+
+}
